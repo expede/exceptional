2787d40f2c412758e5b268bf181acbffc565adf5
Brooklyn Zelenka <bez@brooklynzelenka.com>
2016-08-28 21:13:13 +0900

    Branching condition

--- lib/exceptional/control.ex
+++ lib/exceptional/control.ex
@@ -5,19 +5,17 @@ defmodule Exceptional.Control do
     import __MODULE__
   end
 
+  defdelegate value ~> continue, to: :exception_or_continue
+
   @doc ~S"""
   If an exception, return the exception, otherwise continue computation.
   Essentially an `Either` construct for `Exception`s.
   """
-  @spec value_or_exception(Exception.t | any, fun) :: Exception.t | any
+  @spec exception_or_continue(Exception.t | any, fun) :: Exception.t | any
   defmacro value_or_exception(check_value, continue) do
     quote do
       value = unquote(check_value)
       if Exception.exception?(value), do: value, else: value |> unquote(continue)
     end
-  end
-
-  defmacro value ~> continue do
-    quote do: value_or_exception(unquote(value), unquote(continue))
   end
 end
