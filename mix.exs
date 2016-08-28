4751c71fed9862bf081ec426e1f8f36c23b13980
Brooklyn Zelenka <bez@brooklynzelenka.com>
2016-08-29 04:40:39 +0900

    Bump version number

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Exceptional.Mixfile do
       name:    "Exceptional",
       description: "Helpers for Elixir exceptions",
 
-      version: "0.1.0",
+      version: "1.0.0",
       elixir:  "~> 1.3",
 
       source_url:   "https://github.com/expede/exceptional",
