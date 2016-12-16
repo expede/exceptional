89dddd82dda08d4bf86b3a1c07390cf2bbaaf865
Brooklyn Zelenka <be.zelenka@gmail.com>
2016-12-15 22:47:26 -0800

    Fix generate_args

--- lib/exceptional/unraise.ex
+++ lib/exceptional/unraise.ex
@@ -3,19 +3,18 @@ defmodule Exceptional.Unraise do
   """
 
   defmacro unraise(dangerous_fun) do
-    # quote do
-    #   {fun_name, ctx, _args} = dangerous_fun
-    #   {:arity, arity} = :erlang.fun_info(fun_name, :arity)
-    #   args = generate_args(arity)
+    quote do
+      {:arity, arity} = :erlang.fun_info(unquote(dangerous_fun), :arity)
+      args = generate_args(arity)
 
-    #   fn unquote_splicing(args) ->
-    #     try do
-    #       unquote(dangerous_fun)(unquote_splicing(args))
-    #     rescue
-    #       exception -> exception
-    #     end
-    #   end
-    # end
+      fn unquote_splicing(quote(args)) ->
+        try do
+          unquote(dangerous_fun)(unquote_splicing(args))
+        rescue
+          exception -> exception
+        end
+      end
+    end
   end
 
   def generate_args(arity) do
