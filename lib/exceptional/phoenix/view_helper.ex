c5bb92df579d1054c3dfc8a29cb60ff17890454d
Brooklyn Zelenka <be.zelenka@gmail.com>
2016-12-18 03:02:50 -0800

    Couple of doctests

--- lib/exceptional/phoenix/view_helper.ex
+++ lib/exceptional/phoenix/view_helper.ex
@@ -5,6 +5,10 @@ defmodule Exceptional.Phoenix.ViewHelper do
 
   @formats ~w(html json)a
 
+  @type full     :: %{error: String.t, reason: String.t}
+  @type simple   :: %{error: String.t}
+  @type response :: simple | full
+
   @doc ~S"""
   Generate simple views for Elixir exceptions
   """
@@ -14,25 +18,75 @@ defmodule Exceptional.Phoenix.ViewHelper do
     do: String.t
   ) :: String.t | map
   defmacro defrender(:error, for: http_code, do: base_message) do
-    render("error", for: http_code, only: @formats, do: base_message)
+    render(:error, for: http_code, only: @formats, do: base_message)
   end
 
   defmacro defrender(:error, for: http_code, except: except, do: base_message) do
     only = Enum.reject(@formats, fn format -> Enum.member?(except, format) end)
-    render("error", for: http_code, only: only, do: base_message)
+    render(:errors, for: http_code, only: only, do: base_message)
   end
 
   defmacro defrender(:error, for: http_code, only: formats, do: base_message) do
-    render("error", for: http_code, only: formats, do: base_message)
+    render(:errors, for: http_code, only: formats, do: base_message)
   end
 
-  def render("error", for: http_code, only: formats, do: base_message) do
+  @doc ~S"""
+  Generate several render error functions from a whitelist (`only`)
+
+  ## Examples
+
+      iex> render(:errors, for: 404, only: [:json], do: "Oh no!")
+      [
+        {
+          :def,
+          [context: Exceptional.Phoenix.ViewHelper, import: Kernel],
+          [
+            {
+              :render,
+              [context: Exceptional.Phoenix.ViewHelper],
+              [
+                "404.json",
+                {:error_info, [], Exceptional.Phoenix.ViewHelper}
+              ]
+            },
+            [do: {
+              :render, [], [
+                {
+                  :<<>>, [], [
+                    {
+                      :::,
+                      [],
+                      [
+                        {
+                          {:., [], [Kernel, :to_string]},
+                          [],
+                          [:json]
+                        },
+                        {:binary, [], Exceptional.Phoenix.ViewHelper}
+                      ]
+                    }
+                  ]
+                },
+                "Oh no!",
+                {:error_info, [], Exceptional.Phoenix.ViewHelper}
+              ]
+            }]
+          ]
+        }
+      ]
+
+  """
+  @spec render(:errors, for: non_neg_integer, only: [atom], do: String.t)
+    :: response
+  def render(:errors, for: http_code, only: formats, do: base_message) do
     Enum.map(formats, fn format ->
-      render("error", for: http_code, format: format, do: base_message)
+      render(:error, for: http_code, format: format, do: base_message)
     end)
   end
 
-  def render("error", for: http_code, format: format, do: base_message) do
+  @spec render(String.t, for: non_neg_integer, format: atom, do: String.t)
+    :: response
+  def render(:error, for: http_code, format: format, do: base_message) do
     template = "#{http_code}.#{format}"
 
     quote do
@@ -42,7 +96,17 @@ defmodule Exceptional.Phoenix.ViewHelper do
     end
   end
 
-  def render("html", base_message, error_info) do
+  @doc ~S"""
+  Concrete render of an error in a particular format
+
+  ## Examples
+
+      iex> render(:json, "I'm sorry, I can't do that Dave", %{})
+      %{error: "I'm sorry, I can't do that Dave"}
+
+  """
+  @spec render(:html | :json, String.t, map) :: response
+  def render(:html, base_message, error_info) do
     case error_info do
       %{conn: %{assigns: %{reason: %{message: detail}}}} ->
         "#{base_message}: #{detail}"
@@ -51,7 +115,7 @@ defmodule Exceptional.Phoenix.ViewHelper do
     end
   end
 
-  def render("json", base_message, error_info) do
+  def render(:json, base_message, error_info) do
     case error_info do
       %{conn: %{assigns: %{reason: %{message: detail}}}} ->
         %{error: base_message, reason: detail}
