4ea4d7046488f184689ac2c2f25dc37e5c967a05
Brooklyn Zelenka <be.zelenka@gmail.com>
2016-12-17 18:38:08 -0800

    It's hacky but it works :P

--- lib/exceptional/unraise.ex
+++ lib/exceptional/unraise.ex
@@ -2,25 +2,31 @@ defmodule Exceptional.Unraise do
   @moduledoc ~S"""
   """
 
-  defmacro unraise(dangerous_fun) do
-    quote do
-      {:arity, arity} = :erlang.fun_info(unquote(dangerous_fun), :arity)
-      args = generate_args(arity)
+  defdelegate lower(dangeroud_fun),          to: __MODULE__, as: :unraise
+  defdelegate lower(dangeroud_fun, dynamic), to: __MODULE__, as: :unraise
 
-      fn unquote_splicing(quote(args)) ->
-        try do
-          unquote(dangerous_fun)(unquote_splicing(args))
-        rescue
-          exception -> exception
-        end
-      end
+  def unraise(dangerous_fun) do
+    safe_fun = unraise(dangerous_fun, :dynamic)
+    {:arity, arity} = :erlang.fun_info(dangerous_fun, :arity)
+
+    case arity do
+      0 -> fn ()                 -> safe_fun.([]) end
+      1 -> fn (a)                -> safe_fun.([a]) end
+      2 -> fn (a, b)             -> safe_fun.([a, b]) end
+      3 -> fn (a, b, c)          -> safe_fun.([a, b, c]) end
+      4 -> fn (a, b, c, d)       -> safe_fun.([a, b, c, d]) end
+      5 -> fn (a, b, c, d, e)    -> safe_fun.([a, b, c, d, e]) end
+      6 -> fn (a, b, c, d, e, f) -> safe_fun.([a, b, c, d, e, f]) end
     end
   end
 
-  def generate_args(arity) do
-    [97]
-    |> Stream.unfold(fn acc -> {acc, [97 | acc]} end)
-    |> Enum.take(arity)
-    |> Enum.map(&List.to_atom/1)
+  def unraise(dangerous_fun, :dynamic) do
+    fn arg_list ->
+      try do
+        Kernel.apply(dangerous_fun, arg_list)
+      rescue
+        exception -> exception
+      end
+    end
   end
 end
